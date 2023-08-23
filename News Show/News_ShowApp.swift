06af38c58dcb22d05c395d420666858b8d1693a3
Youssef0Eldeeb <youssefeldeeb3109@gmail.com>
2023-08-23 11:40:35 +0300

    Initial Commit

---
+++ News Show/News_ShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_ShowApp.swift
+//  News Show
+//
+//  Created by Youssef Eldeeb on 23/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct News_ShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
